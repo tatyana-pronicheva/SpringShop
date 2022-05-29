fb4ddb6c9b5134a143e1a6b945038bec0f698748
tpronicheva <tpronicheva@smartlabs.tv>
2022-05-29 10:04:03 +0300

    Initial commit

---
+++ src/test/java/ru/gb/SpringShop/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.SpringShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
